70846aa02f3d9c3224ccf441504e101dce65854d
ny <64143453+nyaoouo@users.noreply.github.com>
2025-02-04 15:46:39 +0800

    add web api

--- src/routes/web_api/server.ts
+++ src/routes/web_api/server.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { setServerTime } from "../../utils";
+import { givePlayerCharacterSync } from "../../lib/character";
+import { givePlayerEquipmentSync } from "../../lib/equipment";
 
 interface TimeQuery {
     time: string | undefined
@@ -30,6 +32,61 @@ const routes = async (fastify: FastifyInstance) => {
             setServerTime(time)
 
         } catch (error) { }
+
+        return reply.redirect(`/`);
+    })
+
+    fastify.get("/give_chara", async (request: FastifyRequest, reply: FastifyReply) => {
+        const q = (request.query as {playerId?:string, characterId?:string});
+        if (!q.playerId || !q.characterId) return reply.status(400).send({
+            "error": "Bad Request",
+            "message": "Invalid query parameters."
+        });
+
+        const playerId = parseInt(q.playerId);
+        const characterId = parseInt(q.characterId);
+        if (isNaN(playerId) || isNaN(characterId)) return reply.status(400).send({
+            "error": "Bad Request",
+            "message": "Invalid query parameters."
+        });
+
+        const giveResult = givePlayerCharacterSync(playerId, characterId);
+        if (!giveResult) return reply.status(400).send({
+            "error": "Bad Request",
+            "message": "Could not give player character."
+        });
+
+        return reply.redirect(`/`);
+    })
+
+    fastify.get("/give_equipment", async (request: FastifyRequest, reply: FastifyReply) => {
+        const q = (request.query as {playerId?:string, equipmentId?:string, amount?:string});
+        if (!q.playerId || !q.equipmentId) return reply.status(400).send({
+            "error": "Bad Request",
+            "message": "Invalid query parameters."
+        });
+
+        const playerId = parseInt(q.playerId);
+        const equipmentId = parseInt(q.equipmentId);
+        if (isNaN(playerId) || isNaN(equipmentId)) return reply.status(400).send({
+            "error": "Bad Request",
+            "message": "Invalid query parameters."
+        });
+
+        let amount = 1;
+        if (q.amount) {
+            amount = parseInt(q.amount);
+            if (isNaN(amount)) return reply.status(400).send({
+                "error": "Bad Request",
+                "message": "Invalid query parameters."
+            });
+        }
+
+        const giveResult = givePlayerEquipmentSync(playerId, equipmentId, amount);
+        if (!giveResult) return reply.status(400).send({
+            "error": "Bad Request",
+            "message": "Could not give player equipment."
+        });
 
         return reply.redirect(`/`);
     })
